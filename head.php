db4c440d80a7cf7032649761b797430d9da16536
MagicMarvMan <marvin@marvnet.de>
2017-12-22 15:51:16 +0100

    Updated gitignore

--- head.php
+++ head.php
@@ -7,10 +7,14 @@
 	<meta name="generator" content="MarvNet's course-interface">
 	<meta name="viewport"  content="width=device-width,initial-scale=1,user-scalable=yes">
 	<title><?php echo $config["site-title"]; ?></title>
-  <link rel="stylesheet" href="/static/lib/bootstrap/v4-imported/css/bootstrap.min.css">
+  <!--<link rel="stylesheet" href="/static/lib/bootstrap/v4-imported/css/bootstrap.min.css">-->
+  <!--<link rel="stylesheet" href="/static/lib/bootstrap/material/css/bootstrap-material-design.min.css">-->
   <script src="/static/lib/jquery/3.2.1-new/jquery-3.2.1.min.js"></script>
   <script src="/static/lib/popperjs/popper.min.js"></script>
-  <script src="/static/lib/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+  <!--<script src="/static/lib/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>-->
+  <!--<script src="/static/lib/bootstrap/material/js/bootstrap-material-design.min.js"></script>-->
+  <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.0.0-beta.4/dist/css/bootstrap-material-design.min.css" integrity="sha384-R80DC0KVBO4GSTw+wZ5x2zn2pu4POSErBkf8/fSFhPXHxvHJydT0CSgAP2Yo2r4I" crossorigin="anonymous">
+<script src="https://unpkg.com/bootstrap-material-design@4.0.0-beta.4/dist/js/bootstrap-material-design.js" integrity="sha384-3xciOSDAlaXneEmyOo0ME/2grfpqzhhTcM4cE32Ce9+8DW/04AGoTACzQpphYGYe" crossorigin="anonymous"></script>
   <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
   <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
   <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
@@ -30,104 +34,32 @@
   </style>
 </head>
 <body>
-<header>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="/"><?php echo $settings->get("site-title"); ?></a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="/index.php">Startseite</a></li>
-      <li><a href="/events.php">Veranstaltungen</a></li>
-      <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Mein Konto<b class="caret"></b></a>
-      	<ul class="dropdown-menu">
-      		<?php
-      			if($loggedIn) {
-      				echo "<li><a href='/account'>Mein Konto</a></li>";
-      				echo "<li><a href='/account/logout'>Logout</a></li>";
-      				echo "<li class='divider'></li>";
-      				echo "<li><a href='/account'>Willkommen, ".$user->getUserData()["username"]."!</a></li>";
-      			} else {
-      				echo "<li><a href='/account/login?return=".$_SERVER['PHP_SELF']."'>Anmelden</a></li>";
-      				echo "<li><a href='/account/register?return=".$_SERVER['PHP_SELF']."'>Registrieren</a></li>";
-      			}
-      		?>	
-      	</ul>	
-      </li>
-      <?php
-        $adminFrames = 0;
-        $admin = array();
-        $admin["users"] = false;
-        $admin["permissions"] = false;
-        $admin["files"] = false;
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#"><?= $settings->get("site-title"); ?></a>
+      <button style="cursor:pointer;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        if($user->hasPermission("admin.files")) {
-          ++$adminFrames;
-          $admin["files"] = true;
-        }
-
-        if($user->hasPermission("admin.users")) {
-          ++$adminFrames;
-          $admin["users"] = true;
-        }
-
-        if($user->hasPermission("admin.permissions")) {
-          ++$adminFrames;
-          $admin["permissions"] = true;
-        }
-
-        if($adminFrames >= 1) {
-        ?>
-        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin<b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <?php if($admin["users"]) { ?>
-            <li><a href="/acp/users">Nutzerverwaltung</a></li>
-            <?php
-          }
-          if( $admin["permissions"]) {
-          ?>
-          <li><a href="/acp/permissions">Rechteverwaltung</a></li>
-          <?php
-        }
-          if($admin["files"]) {
-            ?>
-              <li><a href="/acp/files">Dateiverwaltung</a></li>
-            <?php
-          }
-        ?>
-          </ul>
-        </li>
-
-        <?php
-      }
-      ?>
-    </ul>
-    <?php
-      if(!$loggedIn) {
-      ?>
-      <ul class="nav navbar-nav navbar-right">
-      	<li><a href="/account/register?goto=<?= $_SERVER['PHP_SELF']; ?>"><i class="far fa-user"></i> Registrieren</a></li>
-      	<li><a href="/account/login?goto=<?= $_SERVER['PHP_SELF']; ?>"><i class="fas fa-user"></i> Anmelden</a></li>
-      </ul>
-      <?php
-  		} else {
-  	?>
-<ul class="nav navbar-nav navbar-right" style="margin-right:5px;">
-        <?php $temp = new UserProfile($user->getId(),"id"); ?>
-        <li><?php
-        
-        echo $temp->getRoleTile(); ?></li>
-      </ul>
-    <?php
-  }
-  ?>
-  </div>
-</nav>
-</header>
-<main>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="/account" id="menu-dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mein Konto</a>
+            <div class="dropdown-menu" aria-labelledby="mebu-dropdown01">
+              <a class="dropdown-item" href="/account/login">Anmelden</a>
+              <a class="dropdown-item" href="/account/register">Registrieren</a>
+              <a class="dropdown-item" href="/account/">Mein Konto</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
 <div class="container">
-<noscript>
-  <div class="alert alert-danger">
-    <strong>Achtung!</strong> Diese Webseite funktioniert ohne <strong>JavaScript</strong> nicht korrekt!
-  </div>
-</noscript>
