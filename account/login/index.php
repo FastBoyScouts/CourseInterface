0263bdf117e00a574dbd2acb9f420258192a93e5
MagicMarvMan <marvin@marvnet.de>
2017-12-23 16:17:13 +0100

    Materialized login form

--- account/login/index.php
+++ account/login/index.php
@@ -4,21 +4,23 @@
 <?php
 ?>
 <h3 id="loginMessage"><?php echo $loginMessage; ?></h3>
-<form action="?loginRequest=1" method="POST" id="loginForm">
+<form action="?loginRequest=1" method="POST" id="loginForm" class="my-2 my-lg-0">
 	<input type="hidden" name="login" value="1">
 	<div class="form-group">
-		<label for="username">Nutzername:</label>
-		<input type="text" class="form-control" id="username" name="username" required="required" value="<?= $predefUsername; ?>">
+		<!--<label for="username">Nutzername:</label>-->
+		<input type="text" class="form-control form-text form-control-lg" id="username" name="username" placeholder="Benutzername" required="required" aria-describedby="usernameHelp" value="<?= $predefUsername; ?>">
+		<small id="usernameHelp" class="form-text text-muted">Bitte geben sie hier ihre E-Mail-Adresse ein.</small>
 	</div>
 	<div class="form-group">
-		<label for="password">Passwort:</label>
-		<input type="password" class="form-control" id="password" name="password" required="required" value="<?= $predefPassword; ?>">
+		<!--<label for="password">Passwort:</label>-->
+		<input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Passwort" aria-describedby="passwordHelp" required="required" value="<?= $predefPassword; ?>">
+		<small id="passwordHelp" class="form-text text-muted">Bitte geben sie hier ihr Passwort ein.</small>
 	</div>
 	<div class="form-group">
 		<label>Captcha:</label><br>
 		<div class="g-recaptcha" data-sitekey="6LeucDwUAAAAAGhPmwiLA7h09l2yGcPS6HMPBx3V"></div>
 	</div>
-	<a href="javascript:void(0);" onclick="javascript:submitForm();" class="btn btn-default btn-primary">Submit</a>&nbsp;&nbsp;<a href="/account/register" class="btn btn-secondary">Registrieren</a>
+	<a href="javascript:void(0);" onclick="javascript:submitForm();" class="btn btn-default btn-primary">Anmelden</a>&nbsp;&nbsp;<a href="/account/register" class="btn btn-secondary">Registrieren</a>
 </form>
 <script>function validateForm() {var isHuman = grecaptcha.getResponse();var fields = ["username","password"];var abort = false;fields.forEach(function(elem) {if($('#'+elem).val().length < 1) {abort = true;}});if(abort) {$('#loginMessage').html("<span style='color:red;'>Bitte füllen sie alle Felder aus!</span>");return false;}if(isHuman.length >= 1) {return true;} else {$('#loginMessage').html("<span style='color:red;'>Bitte aktivieren sie den \"Ich bin kein Roboter\"-Haken!</span>");return false;}}function submitForm() {if(validateForm()) {$('#loginForm').submit();} else {$('#loginMessage').html("<span style='color:red;'>Bitte aktivieren sie den \"Ich bin kein Roboter\"-Haken!</span>");}}</script>
 <?php
