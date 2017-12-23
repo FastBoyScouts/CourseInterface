f945dc78b7bfea8df555cba25c6be3aa8b3f6ccc
MagicMarvMan <marvin@marvnet.de>
2017-12-23 19:16:03 +0100

    Removed unneccessary elements from navbar

--- head.php
+++ head.php
@@ -43,13 +43,7 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
+            <a class="nav-link" href="/">Home</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="/account" id="menu-dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mein Konto</a>
